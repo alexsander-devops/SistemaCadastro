cc042652c69775ec227074553e7648339283224a
Alexsander-Farias <alexsander.devops@hotmail.com>
2022-01-23 19:15:25 -0400

    Sistema de CadastroEventos, referente ao exercício Atividade online 2 - senai

---
+++ CadastroEventos.js
@@ -0,0 +1,54 @@
+let Oradores = 10
+
+if(Oradores <= 10) {
+    console.log("Número de Oradores permitido, prossiga com o cadastro!")
+} else {
+    console.log("Número de Oradores excedido, evento não será cadastrado!.")
+}
+
+let participantes = 100
+
+if(participantes <= 100) {
+    console.log ("Número de participantes permitido, prossiga com o cadastro!")
+} else {
+    console.log("Número de participantes excedido, evento não será cadastrado!.")
+}
+
+let idade = '18'
+
+console.log("Sua idade é:", idade.length)
+
+if(idade.length >= 18) {
+    console.log("Cadastro permitido, registre o evento")
+} else {
+    console.log("Cadastro não permitido")
+}
+
+let idadeOradores = '18'
+
+console.log("Idade dos Oradores é:", idadeOradores.length)
+
+if(idade.length >= 18) {
+    console.log("Cadastro permitido ao orador, registre o evento")
+} else {
+    console.log("Cadastro não permitido")
+}
+
+let data = '20'
+
+console.log("A data para o evento é até o dia:", data.length)
+
+if(data.length > 20) {
+    console.log("Evento validado, continue o cadastro")
+} else {
+    console.log("Data inválida, cadastro não permitido")
+}
+
+let Participantes = ['']
+
+console.log(listarparticipantes.length)
+
+let listarOradores = ['']
+
+console.log(listarOradores.length)
+
